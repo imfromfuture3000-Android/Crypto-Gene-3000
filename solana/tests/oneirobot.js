2d3536175d313277995244c6a1e64dc341658f32
Imfromfuture3000 <imfromfuture3000@gmail.com>
2025-08-22 23:19:15 +0800

    Create oneirobot.js

---
+++ solana/tests/oneirobot.js
@@ -0,0 +1,9 @@
+const anchor = require("@coral-xyz/anchor");
+
+describe("oneirobot", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  it("builds", async () => {
+    // placeholder test
+  });
+});
